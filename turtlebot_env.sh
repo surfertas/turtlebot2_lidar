fc4365decb43f657680886c0cff5ed38f3cb626a
Tasuku Miura <tasuku@gmail.com>
2020-03-17 20:23:56 +0800

    env

---
+++ turtlebot_env.sh
@@ -0,0 +1,6 @@
+export TURTLEBOT_SENSOR=rplidar_a2
+export TURTLEBOT_BASE=kobuki
+export TURTLEBOT_STACKS=hexagons
+export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0
+export TURTLEBOT_BATTERY=/proc/acpi/battery/BAT0
+export TURTLEBOT_SIMULATION=false
